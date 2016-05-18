14b6b1f7d740d5ee3cd472f897026f4115df6732
SofiaReis <sofiareis1994@gmail.com>
2016-05-18 22:50:58 +0100

    symbol table done

---
+++ src/parser/STC.java
@@ -0,0 +1,14 @@
+package parser;
+
+public class STC extends Object {
+
+
+	  String type;  // type,in calculator expample there only is one type==> Int.
+	  String value; // value, the value in here is a string not a integer value.
+
+	  public STC(String itype, String ivalue) {
+	    type=itype;
+	    value=ivalue;
+
+	  }
+	}
